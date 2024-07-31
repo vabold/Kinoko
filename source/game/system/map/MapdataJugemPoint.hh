b0f8ef5f63f9f2c705cd7cfed618dfeed6de1760
malleoz <seanowens@comcast.net>
2024-07-31 19:55:09 -0400

    Add JugemPoint infrastructure

---
+++ source/game/system/map/MapdataJugemPoint.hh
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "game/system/map/MapdataAccessorBase.hh"
+
+#include <egg/math/Vector.hh>
+
+namespace System {
+
+/// @brief This class represents the course's respawn positions.
+class MapdataJugemPoint {
+public:
+    struct SData {
+        EGG::Vector3f pos;
+        EGG::Vector3f rot;
+        u8 _18[0x1c - 0x18];
+    };
+    static_assert(sizeof(SData) == 0x1c);
+
+    MapdataJugemPoint(const SData *data);
+    void read(EGG::Stream &stream);
+
+    /// @beginGetters
+    const EGG::Vector3f &pos() const;
+    const EGG::Vector3f &rot() const;
+    /// @endGetters
+
+private:
+    const SData *m_rawData;
+    EGG::Vector3f m_pos;
+    EGG::Vector3f m_rot;
+};
+
+class MapdataJugemPointAccessor
+    : public MapdataAccessorBase<MapdataJugemPoint, MapdataJugemPoint::SData> {
+public:
+    MapdataJugemPointAccessor(const MapSectionHeader *header);
+    ~MapdataJugemPointAccessor() override;
+};
+
+} // namespace System
