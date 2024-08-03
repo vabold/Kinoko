df381b4a7b2710f90b62652f007878d0ae211b55
Aiden <86704247+vabold@users.noreply.github.com>
2024-08-03 14:19:43 -0400

    Add ObjFlow.bin support

---
+++ source/game/field/ObjectFlowTable.hh
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <Common.hh>
+
+namespace Field {
+
+struct SObjectCollisionSet {
+    u16 id;
+    char name[32];
+    char resources[64];
+    u8 _62[0x6a - 0x62];
+    s16 mode;
+    union {
+        struct {
+            s16 radius;
+        } sphere;
+        struct {
+            s16 radius;
+            s16 height;
+        } cylinder;
+        struct {
+            s16 x;
+            s16 y;
+            s16 z;
+        } box;
+    } params;
+    u8 _72[0x74 - 0x72];
+};
+static_assert(sizeof(SObjectCollisionSet) == 0x74);
+
+class ObjectFlowTable {
+public:
+    ObjectFlowTable(const char *filename);
+    ~ObjectFlowTable();
+
+private:
+    struct SFile {
+        s16 count;
+        SObjectCollisionSet sets[];
+    };
+
+    s16 m_count;
+    SObjectCollisionSet *m_sets;
+    s16 *m_slots;
+};
+
+} // namespace Field
