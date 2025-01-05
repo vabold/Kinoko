f67f8d1dd4fb1bc3f1645010f6a23f611a7c6057
malleoz <seanowens@comcast.net>
2025-01-05 02:16:30 -0500

    Implement MapdataPointInfo

---
+++ source/game/system/map/MapdataPointInfo.hh
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "game/system/map/MapdataAccessorBase.hh"
+
+#include <egg/math/Vector.hh>
+#include <egg/util/Stream.hh>
+
+#include <span>
+
+namespace System {
+
+class MapdataPointInfo {
+public:
+    struct Point {
+        EGG::Vector3f pos;
+        u16 setting[2];
+    };
+
+    struct SData {
+        u16 pointCount;
+        u8 settings[2];
+        Point points[];
+    };
+    STATIC_ASSERT(sizeof(SData) == 0x4);
+
+    MapdataPointInfo(const SData *data);
+
+    void read(EGG::RamStream &stream);
+
+    [[nodiscard]] size_t pointCount() const;
+
+private:
+    const SData *m_rawData;
+    std::array<u8, 2> m_settings;
+    std::span<Point> m_points;
+};
+
+class MapdataPointInfoAccessor
+    : public MapdataAccessorBase<MapdataPointInfo, MapdataPointInfo::SData> {
+public:
+    MapdataPointInfoAccessor(const MapSectionHeader *header);
+    ~MapdataPointInfoAccessor() override;
+
+    void init(const MapdataPointInfo::SData *start, u16 count);
+};
+
+} // namespace System
