5550a5dd1237a0b2550a1c8c2837ba7e6bdede02
malleoz <seanowens@comcast.net>
2025-01-12 16:05:06 -0500

    Add ObjectKCL and implement Mdush

--- source/game/field/obj/ObjectRegistry.hh
+++ source/game/field/obj/ObjectRegistry.hh
@@ -2,6 +2,7 @@
 
 #include "game/field/obj/ObjectAurora.hh"
 #include "game/field/obj/ObjectDokan.hh"
+#include "game/field/obj/ObjectKCL.hh"
 #include "game/field/obj/ObjectNoImpl.hh"
 #include "game/field/obj/ObjectOilSFC.hh"
 #include "game/field/obj/ObjectParasolR.hh"
