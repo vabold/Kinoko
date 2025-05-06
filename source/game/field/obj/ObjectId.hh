643a98848dffb867d418c62db15ecd5c64bf351a
malleoz <seanowens@comcast.net>
2025-05-06 02:24:21 -0400

    Implement or blacklist all primitive ObjectCollidable types

--- source/game/field/obj/ObjectId.hh
+++ source/game/field/obj/ObjectId.hh
@@ -10,11 +10,18 @@ enum class ObjectId {
     Boble = 0xdd,
     DokanSFC = 0x12e,
     CastleTree1c = 0x130,
+    MarioTreeGCc = 0x134,
+    PeachTreeGCc = 0x138,
     ObakeBlockSFCc = 0x13c,
     WLDokanGC = 0x13f,
+    MarioGo64c = 0x140,
+    KinokoT1 = 0x142,
     PalmTree = 0x145,
+    Parasol = 0x146,
+    HeyhoTreeGBAc = 0x14a,
     GardenTreeDSc = 0x151,
     DKtreeA64c = 0x158,
+    DKTreeB64c = 0x15a,
     TownTreeDsc = 0x15b,
     OilSFC = 0x15d,
     ParasolR = 0x16e,
@@ -23,6 +30,7 @@ enum class ObjectId {
     Kuribo = 0x191,
     WLFirebarGC = 0x195,
     WLFireRingGC = 0x1a1,
+    PakkunDokan = 0x1a2,
     PuchiPakkun = 0x1aa,
     KinokoUd = 0x1f5,
     KinokoBend = 0x1f6,
@@ -33,8 +41,32 @@ enum class ObjectId {
 
 enum class BlacklistedObjectId {
     Itembox = 0x65,
+    CastleTree2 = 0x131,
+    CastleFlower1 = 0x132,
+    MarioTreeGC = 0x133,
+    DonkyTree1GC = 0x135,
+    DonkyTree2GC = 0x136,
+    PeachTreeGC = 0x137,
+    PeachHunsuiGC = 0x141,
     GardenTreeDS = 0x150,
+    FlagA1 = 0x152,
+    FlagA2 = 0x153,
+    FlagB1 = 0x154,
+    FlagB2 = 0x155,
+    FlagA3 = 0x156,
+    DKTreeA64 = 0x157,
+    DKTreeB64 = 0x159,
+    MiiBalloon = 0x160,
+    Windmill = 0x161,
+    TownTreeDS = 0x163,
     Hanabi = 0x16a,
+    LightHouse = 0x16d,
+    FlagA5 = 0x180,
+    SentakuDS = 0x189,
+    FlagB3 = 0x2c4,
+    FlagB4 = 0x2c7,
+    UtsuboDokan = 0x2d6,
+    EnvSnow = 0x2ef,
 };
 
 static constexpr bool IsObjectBlacklisted(u16 id) {
@@ -45,8 +77,30 @@ static constexpr bool IsObjectBlacklisted(u16 id) {
         return true;
 
     // No collision
+    case BlacklistedObjectId::CastleTree2:
+    case BlacklistedObjectId::CastleFlower1:
+    case BlacklistedObjectId::MarioTreeGC:
+    case BlacklistedObjectId::DonkyTree1GC:
+    case BlacklistedObjectId::DonkyTree2GC:
+    case BlacklistedObjectId::PeachTreeGC:
+    case BlacklistedObjectId::PeachHunsuiGC:
     case BlacklistedObjectId::GardenTreeDS:
+    case BlacklistedObjectId::FlagA1:
+    case BlacklistedObjectId::FlagA2:
+    case BlacklistedObjectId::FlagB1:
+    case BlacklistedObjectId::FlagB2:
+    case BlacklistedObjectId::FlagA3:
+    case BlacklistedObjectId::DKTreeA64:
+    case BlacklistedObjectId::Windmill:
+    case BlacklistedObjectId::TownTreeDS:
     case BlacklistedObjectId::Hanabi:
+    case BlacklistedObjectId::LightHouse:
+    case BlacklistedObjectId::FlagA5:
+    case BlacklistedObjectId::SentakuDS:
+    case BlacklistedObjectId::FlagB3:
+    case BlacklistedObjectId::FlagB4:
+    case BlacklistedObjectId::UtsuboDokan:
+    case BlacklistedObjectId::EnvSnow:
         return true;
 
     default:
