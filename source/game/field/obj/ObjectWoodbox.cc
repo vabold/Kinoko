c942b2224c9e6faedc4e4de07ca5fd23eb77124e
o2161405 <91959604+o2161405@users.noreply.github.com>
2025-08-20 22:35:34 +0100

    Update ObjectWoodbox.cc

--- source/game/field/obj/ObjectWoodbox.cc
+++ source/game/field/obj/ObjectWoodbox.cc
@@ -1,7 +1,5 @@
 #include "ObjectWoodbox.hh"
 
-#include "game/field/CollisionDirector.hh"
-
 namespace Field {
 
 /// @addr{0x8077E5E4}
@@ -13,13 +11,14 @@ ObjectWoodbox::~ObjectWoodbox() = default;
 /// @addr{0x8077EBB8}
 void ObjectWoodbox::calcCollisionTransform() {
     constexpr f32 HALF_SIZE = 100.0f;
+    constexpr EGG::Vector3f POS_OFFSET = EGG::Vector3f(0.0f, HALF_SIZE, 0.0f);
 
     if (!m_collision) {
         return;
     }
 
     calcTransform();
-    EGG::Vector3f nextPos = m_pos + EGG::Vector3f(0.0f, HALF_SIZE, 0.0f);
+    EGG::Vector3f nextPos = m_pos + POS_OFFSET;
     m_transform.setBase(3, nextPos);
     m_collision->transform(m_transform, m_scale, getCollisionTranslation());
 }
