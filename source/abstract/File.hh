2c9fe5786c855d1b9c2ddbecf75617d0fe770dec
em-eight <m8em.eightm8@gmail.com>
2026-02-15 11:37:00 -0600

    Add option to load game files from path specified from KINOKO_FILESYSTEM_ROOT

--- source/abstract/File.hh
+++ source/abstract/File.hh
@@ -4,7 +4,10 @@
 
 namespace Abstract::File {
 
+/// @brief Loads kinoko game files
 [[nodiscard]] u8 *Load(const char *path, size_t &size);
+/// @brief Loads files from host system
+[[nodiscard]] u8 *LoadHost(const char *path, size_t &size);
 void Append(const char *path, const char *data, size_t size);
 int Remove(const char *path);
 
